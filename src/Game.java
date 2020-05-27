6fbe05340c5a74afc3e177fe47ec1fec8ee18204
S36379 <serdar.ayalp@regiocom.com>
2020-05-27 14:21:41 +0200

    First Commit

---
+++ src/Game.java
@@ -0,0 +1,40 @@
+public class Game {
+
+    private int[] values;
+    public static final int[] moves = {-1, 1};
+    private int first, last;
+
+    public Game(int[] values) {
+        this.values = values;
+        first = 0;
+        last = values.length - 1;
+    }
+
+    public boolean isFinal() {
+        return last - first == 1;
+    }
+
+    public int score() {
+        return values[first] - values[last];
+    }
+
+    public void doMove(int move, String player) {
+        if (move < 0) {
+            System.out.println(player + " - MOVE: FIRST: " + values[first]);
+            first++;
+        } else {
+            System.out.println(player + " - MOVE: LAST: " + values[last]);
+            last--;
+        }
+    }
+
+    public void undoMove(int move, String player) {
+        if (move < 0) {
+            //System.out.println(player + " - UNDO-MOVE: FIRST: " + values[first]);
+            first--;
+        } else {
+            //System.out.println(player + " - UNDO-MOVE: LAST: " + values[last]);
+            last++;
+        }
+    }
+}
---
+++ src/MinMaxBestDifference.java
@@ -0,0 +1,74 @@
+public class MinMaxBestDifference {
+
+    private Game game;
+
+    private static int player_a_counter = 0;
+    private static int player_b_counter = 0;
+
+    public MinMaxBestDifference(int[] values) {
+        game = new Game(values);
+    }
+
+    public int scorePlayerA() {
+
+        if (game.isFinal()) { // Lösung (Ende) erreicht?
+            System.out.println("SCORE: " + game.score());
+            System.out.println("");
+            return game.score();
+        }
+
+        int maxScore = Integer.MIN_VALUE;
+
+        for (int move : Game.moves) { // alle Möglichkeiten durchlaufen
+
+            ++player_a_counter;
+            System.out.println();
+            System.out.println("**************** SCHLEIFE - PLAYER - A: " + player_a_counter);
+
+            game.doMove(move, "PLAYER-A"); // Zug ausführen
+            int score = scorePlayerB(); // alternierend rekursiver Aufruf
+            game.undoMove(move,"PLAYER-A"); // Zug rückgängig machen
+            if (score > maxScore) // Maximum der Bewertungen
+                maxScore = score;
+        }
+        return maxScore;
+    }
+
+    public int scorePlayerB() {
+
+        if (game.isFinal()) {
+            System.out.println("SCORE: " + game.score());
+            System.out.println("");
+            return game.score();
+        }
+
+        int minScore = Integer.MAX_VALUE;
+
+        for (int move : Game.moves) {
+
+            ++player_b_counter;
+            System.out.println();
+            System.out.println("**************** SCHLEIFE - PLAYER - B: " + player_b_counter);
+
+            game.doMove(move, "PLAYER-B");
+            int score = scorePlayerA();
+            game.undoMove(move, "PLAYER-B");
+            if (score < minScore)
+                minScore = score;
+        }
+        return minScore;
+    }
+
+    public static void main(String[] args) {
+
+        int[] values = {2, 8, 3, 5, 4, 1};
+
+        MinMaxBestDifference bd = new MinMaxBestDifference(values);
+
+        int bestScoreA = bd.scorePlayerA();
+        System.out.println(bestScoreA);
+
+        /*int bestScoreB = bd.scorePlayerB();
+        System.out.println(bestScoreB);*/
+    }
+}
